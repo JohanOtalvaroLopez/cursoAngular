bdc8a93dbe37533fb341f5ae5f8f2240bec9d609
Johan Otalvaro Lopez <jfotalva@bancolombia.com.co>
2024-08-01 15:01:51 -0500

    Avance 1 - cursoAngular

--- src/app/maestros/accounts/accounts.component.ts
+++ src/app/maestros/accounts/accounts.component.ts
@@ -17,12 +17,13 @@ export class AccountsComponent implements OnInit {
   lstrUser: string = '';
 
   ngOnInit(): void {
+    this.lstrUser = this.service.lstrUser; 
     this.fnGetAccountsComponent();
   }
 
   fnGetAccountsComponent() {
-    //this.dataService.fnGetAccountsDataServices(this.lstrUser).subscribe({ // --> Solo busca con el INPUT pero no con el del LOGIN(Robin)
-    this.dataService.fnGetAccountsDataServices(this.service.lstrUser).subscribe({ // --> Se va siempre con ROBIN(LOGIN)
+    this.dataService.fnGetAccountsDataServices(this.lstrUser).subscribe({ // --> Solo busca con el INPUT pero no con el del LOGIN(Robin)
+    //this.dataService.fnGetAccountsDataServices(this.service.lstrUser).subscribe({ // --> Se va siempre con ROBIN(LOGIN)
       next: (res) => {
         this.accounts = res;
       },
